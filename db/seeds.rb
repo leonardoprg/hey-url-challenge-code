9cbeb9d0964a2f90db55d069779f6217bd831bce
Leonardo Siqueira <leonardo.prg@gmail.com>
2021-02-05 14:53:54 -0300

    adding some seed for url and click

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+url = Url.create!(original_url: 'http://www.google.com')
+url.clicks.create!(browser: 'Firefox', platform: 'OSX', created_at: 1.day.ago)
+url.clicks.create!(browser: 'Firefox', platform: 'OSX')
+url.clicks.create!(browser: 'Chrome', platform: 'Linux')
